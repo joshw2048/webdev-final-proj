420db7fa43302e0be1f71367617752ad08d8b540
Azucarheart Clues <connietliu2020@gmail.com>
2024-02-07 19:30:53 -0500

    arrays, json

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -1,9 +1,11 @@
 import BooleanVariables from "./variables/BooleanVariables";
 import IfElse from "./conditionals/IfElse";
-import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import JsonStringify from "./json/JsonStringify";
 import TernaryOperator from "./conditionals/TernaryOperator";
 import VariablesAndConstants from "./variables/VariablesAndConstants";
 import VariableTypes from "./variables/VariableTypes";
+import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
   
 function JavaScript() {
   console.log('Hello World!');
@@ -16,6 +18,8 @@ function JavaScript() {
         <IfElse />
         <TernaryOperator />
         <WorkingWithFunctions />
+        <WorkingWithArrays />
+        <JsonStringify />
      </div>
   );
 }
