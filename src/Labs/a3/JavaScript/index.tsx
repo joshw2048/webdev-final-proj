59b3f483529621990752c5c02e68042680e24dbf
Azucarheart Clues <connietliu2020@gmail.com>
2024-02-07 19:08:57 -0500

    javascript part 1

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -1,8 +1,21 @@
+import BooleanVariables from "./variables/BooleanVariables";
+import IfElse from "./conditionals/IfElse";
+import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import VariablesAndConstants from "./variables/VariablesAndConstants";
+import VariableTypes from "./variables/VariableTypes";
+  
 function JavaScript() {
   console.log('Hello World!');
   return(
      <div>
         <h1>JavaScript</h1>
+        <VariablesAndConstants/>
+        <VariableTypes />
+        <BooleanVariables />
+        <IfElse />
+        <TernaryOperator />
+        <WorkingWithFunctions />
      </div>
   );
 }
