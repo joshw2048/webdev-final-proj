f7b6c27927abaa8551d1a2800e1d4784eba885d2
Azucarheart Clues <connietliu2020@gmail.com>
2024-02-18 12:43:34 -0500

    icons

--- src/Kanbas/Courses/Modules/ListItem.tsx
+++ src/Kanbas/Courses/Modules/ListItem.tsx
@@ -44,5 +44,4 @@ export const ListItem = (props: { module: CourseModule }) => {
       )}
     </li>
   )
-
 }
--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,14 +1,32 @@
 import { Link, useLocation } from "react-router-dom";
+import { FaEyeSlash } from "react-icons/fa";
 import "./index.css"; 
 
 function CourseNavigation() {
-  const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
+  const links = [
+    "Home",
+    "Modules",
+    "Piazza",
+    "Grades",
+    "Assignments",
+    "Quizzes",
+    "Panopto Video",
+    "Discussions",
+    "Announcements",
+    "Collaboratons",
+    "People",
+    "Files",
+    "Rubrics",
+    "Outcomes",
+    "Syllabus",
+    "Settings",
+  ];
   const { pathname } = useLocation();
   return (
     <ul className="wd-navigation">
       {links.map((link, index) => (
         <li key={index} className={pathname.includes(link) ? "wd-active" : ""}>
-          <Link to={link}>{link}</Link>
+          <Link to={link}>{link}{index > 11 && <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>}</Link>
         </li>
       ))}
     </ul>
