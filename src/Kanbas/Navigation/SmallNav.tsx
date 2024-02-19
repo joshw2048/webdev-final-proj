cf51cf5540ca24bf5ac269b7c1c2956eab8e4266
Azucarheart Clues <connietliu2020@gmail.com>
2024-02-18 21:47:09 -0500

    responsiveness

--- src/Kanbas/Navigation/SmallNav.tsx
+++ src/Kanbas/Navigation/SmallNav.tsx
@@ -1,34 +1,50 @@
 import './small-nav.css';
 import { useState } from 'react';
 import { courses } from "../../Kanbas/Database";
-import { 
+import {
   useLocation,
   Link
 } from "react-router-dom";
 import { links } from './index';
-import { FaBars, FaChevronDown, FaChevronRight, FaEyeSlash } from "react-icons/fa";
+import {
+  FaBars,
+  FaChevronDown,
+  FaChevronRight,
+  FaEmpire,
+  FaEyeSlash,
+  FaFile,
+  FaHome,
+  FaMicrophone,
+  FaPaperclip,
+  FaPlug,
+  FaPlus,
+  FaRegFile,
+  FaRegFileAlt,
+  FaRegUser,
+} from "react-icons/fa";
 
 const courseLinks = [
-  { label: "Home", },
-  { label: "Modules", },
-  { label: "Piazza", },
-  { label: "Grades", },
-  { label: "Assignments", },
-  { label: "Quizzes", },
-  { label: "Panopto Video", },
-  { label: "Discussions", },
-  { label: "Announcements", },
-  { label: "Collaboratons", },
-  { label: "People", secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>},
-  { label: "Files", secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>},
-  { label: "Rubrics", secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>},
-  { label: "Outcomes", secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>},
-  { label: "Syllabus", secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>},
-  { label: "Settings", secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2"/>},
+  { label: "Home", icon: <FaHome style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Modules", icon: <FaPaperclip style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Piazza", icon: <FaPlug style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Zoom Meetings", icon: <FaPlug style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Grades", icon: <FaFile style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Assignments", icon: <FaRegFileAlt style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Quizzes", icon: <FaFile style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Panopto Video", icon: <FaPlug style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Discussions", icon: <FaFile style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Announcements", icon: <FaMicrophone style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "Collaboratons", icon: <FaRegUser style={{ color: "#C8102E", marginRight: '2px' }} /> },
+  { label: "People", icon: <FaRegUser style={{ color: "#C8102E", marginRight: '2px'}} />, secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2" /> },
+  { label: "Files", icon: <FaFile style={{ color: "#C8102E", marginRight: '2px'}} />, secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2" /> },
+  { label: "Rubrics", icon: <FaFile style={{ color: "#C8102E", marginRight: '2px'}} />, secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2" /> },
+  { label: "Outcomes", icon: <FaPlus style={{ color: "#C8102E", marginRight: '2px'}} />, secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2" /> },
+  { label: "Syllabus", icon: <FaRegFile style={{ color: "#C8102E", marginRight: '2px'}} />, secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2" /> },
+  { label: "Settings", icon: <FaEmpire style={{ color: "#C8102E", marginRight: '2px'}} />, secondaryIcon: <FaEyeSlash style={{ color: "darkGray" }} className="ms-2" /> },
 ]
 
 export const SmallNav = () => {
-  
+
   const { pathname } = useLocation();
   const pathArray = pathname.includes("Courses") ? pathname.split('/') : [];
   const courseId = pathArray.length > 0 ? pathArray[pathArray.length - 2] : undefined;
@@ -41,29 +57,29 @@ export const SmallNav = () => {
   return (
     <>
       <div className='hamburger-nav'>
-        <button 
-          className="icon-button" 
-          onClick={() => { 
+        <button
+          className="icon-button"
+          onClick={() => {
             setNavOpen(!navOpen)
             setCourseNavOpen(false);
           }}>
           <FaBars />
         </button>
-        {courseId && <span>{course?.description} <FaChevronRight /> {page}</span>} 
-        {courseId && 
-        <button 
-          className="icon-button"
-          onClick={() => { 
-            setCourseNavOpen(!courseNavOpen)
-            setNavOpen(false);
-          }}
-        >
-          <FaChevronDown />
-        </button>}
+        {courseId && <span>{course?.description} <FaChevronRight /> {page}</span>}
+        {courseId &&
+          <button
+            className="icon-button"
+            onClick={() => {
+              setCourseNavOpen(!courseNavOpen)
+              setNavOpen(false);
+            }}
+          >
+            <FaChevronDown />
+          </button>}
       </div>
       {navOpen && <div className='main-nav'>
-        <div style={{ display: 'flex', flexDirection: 'row-reverse'}}>
-          
+        <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+
         </div>
         <ul className="toggle-navigation">
           {links.map((link, index) => (
@@ -73,13 +89,16 @@ export const SmallNav = () => {
                   setCourseNavOpen(false);
                   setNavOpen(false);
                 }}
-              >  
-                {link.icon ? link.icon : null} 
-                {link.label} 
+                style={{
+                  color: "black"
+                }}
+              >
+                {link.icon ? link.icon : null}
+                {link.label}
               </Link>
             </li>
           ))}
-        </ul> 
+        </ul>
       </div>}
       {courseNavOpen && courseId && <div className='course-nav'>
         <ul className="toggle-navigation">
@@ -90,14 +109,17 @@ export const SmallNav = () => {
                   setCourseNavOpen(false);
                   setNavOpen(false);
                 }}
-              >  
-                {/* {link.icon ? link.icon : null}  */}
-                {link.label} 
+                style={{
+                  color: "#C8102E"
+                }}
+              >
+                {link.icon ? link.icon : null}
+                {link.label}
                 {link.secondaryIcon ? link.secondaryIcon : null}
               </Link>
             </li>
           ))}
-        </ul> 
+        </ul>
       </div>}
     </>
   );
