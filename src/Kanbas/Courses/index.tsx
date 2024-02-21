04dd15820b8e1b65923a30b101f77c1a6061ce92
Azucarheart Clues <connietliu2020@gmail.com>
2024-02-21 18:17:22 -0500

    update citation

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -23,9 +23,12 @@ function Courses() {
   const page = pathArray[pathArray.length - 1];
 
   /**
-   * This code to check media queries comes from stackoverflow: https://stackoverflow.com/questions/54491645/media-query-syntax-for-reactjs
+   * This code that functions as a media query breakpoint comes from stackoverflow: https://stackoverflow.com/questions/54491645/media-query-syntax-for-reactjs
+   * from Marcos Guerrero
    * It makes use of two React Hooks, useState and useEffect, in order to determine when the window is resized and hits a certain
    * breakpoint. This code is being used to determine when to show the breadcrumb menu vs the general menu.
+   * 
+   * I changed variable names to make more sense with my needs, but the functionality is mainly the same.
    */
   const [largeScreen, setLargeScreen] = useState(
     window.matchMedia("(min-width: 768px)").matches
