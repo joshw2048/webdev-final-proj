4acb3ec7f70a5609f2f30c57e7cbe19bf304aa61
Azucarheart Clues <connietliu2020@gmail.com>
2024-03-18 14:21:10 -0400

    lab 5 3.1

---
+++ App2.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+const app = express();
+app.use(express.json());
+Lab5(app);
+Hello(app);
+app.listen(4000)
+
+
+// for pre-lab of assignment 5
+// *** note: must cd into src before running nodemon App2.js
